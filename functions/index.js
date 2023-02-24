977c138e12b8133573b4c936bb534fc13b6ca62e
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-24 15:09:01 -0500

    installed jwt

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,15 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { signup } from "./src/users.js";
+import { signup, login } from "./src/users.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-//login and signup routes....
+//login and signup routes....   
 app.post("/signup", signup)
+app.post("/login", login)
 
 
 export const api = functions.https.onRequest(app)
--- functions/src/users.js
+++ functions/src/users.js
@@ -15,3 +15,29 @@ export function signup(req, res) {
             res.status(500).send({ success: false, message: err.message })
         })
 }
+
+export function login(req, res) {
+    const {email, password } = req.body;
+    // const newUser = {
+    //     email: email.toLowerCase(), 
+    //     password: password,
+    // }
+    const db = dbConnect();
+    db.collection('users')
+        .where('email', '==', email.toLowerCase())
+        .where('passowrd', '==', password)
+        .get()
+        .then(collection => {
+            const users = collection.docs.map(doc => ({...doc.data(), userId: doc.id }))
+            if(!users.length) {
+                res.status(401).send({success: false, message: 'User not found'})
+                return
+            }
+            let user = users[0]
+            user.password = undefined 
+            res.status(200).send(users)
+        })
+        .catch(err => {
+            res.status(500).send({ success: false, message: err.message })
+        })
+}
